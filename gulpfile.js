2658f34c552c883e2f35552343c1f2a1656b88ac
Mauricio Sousa <mauriciosouza8989@gmail.com>
2023-11-13 14:50:09 -0300

    setup do projero

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-function teste(cb){
-    console.log("Hello World!");
-    cb()
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function styles(){
+    return gulp.src('./src/styles/*.scss')
+           .pipe(sass({ outputStyle: 'compressed'}))
+           .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = teste;
+exports.default = styles;
+exports.watch = function(){
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+}
